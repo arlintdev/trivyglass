d19e069651c7f1f78727a90f2e80387e0700e6ee
Austin Arlint <aarlint@teamraft.com>
2025-02-11 19:58:56 -0700

    Initial commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+	let { children } = $props();
+	import '../app.css';
+	import NavBar from '../components/NavBar.svelte';
+	import { ChartOutline, GridSolid, MailBoxSolid, UserSolid } from 'flowbite-svelte-icons';
+</script>
+
+<div class="flex flex-col h-screen">
+
+
+	<NavBar ></NavBar>
+	<div class="flex flex-1">
+
+		<div class="flex-1 overflow-auto px-4">
+			<div class="rounded-lg border-2 border-dashed border-gray-200 p-4 dark:border-gray-700">
+				{@render children()}
+			</div>
+		</div>
+	</div>
+</div>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,10 @@
+
+<h1 class="text-3xl font-bold underline">
+    Welcome to Trivy Glass
+  </h1>
+  
+  <style lang="postcss">
+    :global(html) {
+      background-color: theme(colors.gray.100);
+    }
+  </style>
